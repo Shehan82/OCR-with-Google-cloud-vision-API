d9926d084ee9930b03c0cada31d8587d976c9ab3
shehan82 <shehansandeepa82@gmail.com>
2021-05-21 23:51:24 +0530

    sinhala language not supported yet, it is experimental language

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const vision = require("@google-cloud/vision");
+
+const app = express();
+
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: "API_key.json",
+});
+
+client.textDetection("./love2.jpg").then((results) => {
+  const labels = results[0].labelAnnotations;
+  console.log("Labels:");
+  labels.forEach((label) => console.log(label.description));
+
+  console.log(results);
+});
+
+app.listen(8000, "127.0.0.1", () => console.log("server runing"));
