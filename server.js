da08cb2d34a85a321194ed400c2c804d7c609ef1
shehan82 <shehansandeepa82@gmail.com>
2021-05-22 09:14:44 +0530

    added multer and trying get request as image

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const vision = require("@google-cloud/vision");
+const multer = require("multer");
+
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
 
 const app = express();
 
@@ -7,12 +24,16 @@ const client = new vision.ImageAnnotatorClient({
   keyFilename: "API_key.json",
 });
 
-client.textDetection("./love2.jpg").then((results) => {
+client.documentTextDetection("./my.png").then((results) => {
   //   const labels = results[0].labelAnnotations;
   //   console.log("Labels:");
   //   labels.forEach((label) => console.log(label.description));
 
-  console.log(results);
+  console.log(results[0].fullTextAnnotation.text);
+});
+
+app.post("/upload", upload.single("profile"), (req, res) => {
+  console.log(req.file);
 });
 
 app.listen(8000, "127.0.0.1", () => console.log("server runing"));
